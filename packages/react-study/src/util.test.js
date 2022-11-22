b738f09e8835057d0a42a56a5d24bfe8aec5f088
dongeun-i <dongeun1014@naver.com>
2022-11-22 22:24:10 +0900

    주석 추가

--- packages/react-study/src/util.test.js
+++ packages/react-study/src/util.test.js
@@ -1,5 +1,5 @@
 import { addNumber } from "./util";
-
+// it , expect, tobe 는 제스트에서 테스트코드를 작성할때 쓰는 함수
 it("add tow numbers", () => {
     const result = addNumber(1, 2);
     expect(result).toBe(3);
