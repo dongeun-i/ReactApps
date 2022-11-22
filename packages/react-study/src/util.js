f2703ef7679d33d3c2a53449759638265ca40bbc
dongeun-i <dongeun1014@naver.com>
2022-11-22 22:23:12 +0900

    테스트코드 예제

---
+++ packages/react-study/src/util.js
@@ -0,0 +1,4 @@
+// test code 만들어보기
+export function addNumber(a, b) {
+    return a + b;
+}
---
+++ packages/react-study/src/util.test.js
@@ -0,0 +1,6 @@
+import { addNumber } from "./util";
+
+it("add tow numbers", () => {
+    const result = addNumber(1, 2);
+    expect(result).toBe(3);
+});
