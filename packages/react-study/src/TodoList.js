229b63729dee6f295f0d07510246ec107e356464
dongeun-l <dongeun1014@naver.com>
2022-11-26 13:52:04 +0900

    동적 import 사용해보기

---
+++ packages/react-study/src/TodoList.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+export default function TodoList() {
+    const [todos, setTodos] = useState([]);
+    const onClick = () => {
+        import("./Todo").then(({ Todo }) => {
+            const position = todos.length + 1;
+            const newTodo = <Todo key={position} title={`할일 ${position}`} />;
+            setTodos([...todos, newTodo]);
+        });
+    };
+    return (
+        <div>
+            <button onClick={onClick}>할 일 추가</button>
+            {todos}
+        </div>
+    );
+}
