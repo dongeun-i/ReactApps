ad86db1a9f05424e65a8bfbe85f7fd475499356b
dongeun-l <dongeun1014@naver.com>
2022-11-26 13:51:32 +0900

    환경변수 추가해보기

--- packages/react-study/src/App.js
+++ packages/react-study/src/App.js
@@ -1,13 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
-import smallImg from "./small.jpeg";
-import bigImg from "./big.jpeg";
+// import smallImg from "./small.jpeg";
+// import bigImg from "./big.jpeg";
+import TodoList from "./TodoList";
 
 function App() {
+    console.log(`NODE_ENV = ${process.env.NODE_ENV}`);
+    console.log(`REACT_APP_DATA_API = ${process.env.REACT_APP_DATA_API}`);
+    console.log(`REACT_APP_LOGIN_API = ${process.env.REACT_APP_LOGIN_API}`);
     return (
         <div className="App">
-            <img src={smallImg} alt="small"></img>
-            <img src={bigImg} alt="big"></img>
+            {/* <img src={smallImg} alt="small"></img>
+            <img src={bigImg} alt="big"></img> */}
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
@@ -22,6 +26,7 @@ function App() {
                     Learn React
                 </a>
             </header>
+            <TodoList />
         </div>
     );
 }
